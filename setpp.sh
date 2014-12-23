2fd7de7843479bf9001a66cfbe7e70b7cdeea4b2
revoltek <rev@voo.it>
2014-12-23 14:32:37 +0100

    add setpp

---
+++ setpp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+export PYTHONPATH=/home/stsf309/scripts:/home/stsf309/scripts/GMRTpipeline:/home/lofar/local/lib/python/:/home/lofar/local/src/DistCal/:/home/lofar/local/src/LSMTool/:/home/lofar/local/src/LoSoTo/:/home/lofar/local/src/LOFAR/build/gnu_opt/installed/lib/python2.7/dist-packages:/home/lofar/lofar_build/lofar/release/lib/python2.7/site-packages:/home/lofar/lofar_build/local/release/lib/python2.7:/home/lofar/lofar_build/lofar/release/lib/python2.7/dist-packages:
+$@
